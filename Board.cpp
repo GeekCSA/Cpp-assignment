a0d1f9b567c5ca427a4e79f1c7c2c4c74d2265b1
GeekCSA <moshe.hanu@gmail.com>
2018-05-06 21:29:56 +0300

    Exercise 6 - Board

---
+++ Board.cpp
@@ -0,0 +1,113 @@
+/*
+ * Board.cpp
+ *
+ *  Created on: May 3, 2018
+ *      Author: mcsa
+ */
+
+#include "Board.h"
+using namespace std;
+
+Board::Board(int size){
+	create(size);
+
+	for (int i = 0; i < size; ++i) {
+		for (int j = 0; j < size; ++j) {
+			board[i][j] = Cell(i,j);
+		}
+	}
+}
+
+Board::Board(const Board& b){
+
+	create(b.tableSize);
+
+	for(int i = 0; i < tableSize; i++){
+		for(int j = 0; j < tableSize; j++){
+			board[i][j] = b.board[i][j];
+		}
+	}
+}
+
+void Board::create(int size){
+	tableSize = size;
+
+	//Create 2D array of cells
+	board = new Cell*[tableSize];
+	for(int i = 0; i < tableSize; i++)
+		board[i] = new Cell[tableSize];
+}
+
+Board::~Board(){
+	free();
+}
+
+void Board::free(){
+	for(int i = 0; i < tableSize; i++)
+		delete [] board[i];
+
+	delete [] board;
+}
+
+std::ostream & operator<<(std::ostream& os, const Board& b){
+	for(int i = 0; i < b.tableSize; i++){
+		for(int j = 0; j < b.tableSize; j++)
+			os << b.board[i][j];
+		os << endl;
+	}
+
+	return os;
+}
+
+Board& Board::operator=(const Board& b){//TODO if correct
+
+	if(b.tableSize != tableSize){
+		free();
+		create(b.tableSize);
+	}
+
+	for(int i = 0; i < tableSize; i++)
+		for(int j = 0; j < tableSize; j++){
+			board[i][j] = b.board[i][j];
+		}
+
+	return *this;
+}
+
+Board& Board::operator=(const char c){
+
+	if(c == '.' || c == 'X' || c == 'O'){
+		for(int i = 0; i < tableSize; i++)
+			for(int j = 0; j < tableSize; j++)
+				board[i][j] = c;
+		return *this;
+	}
+	else
+		throw std::invalid_argument( "Incorrect value\n" );
+
+}
+
+char Board::operator[](const Cell& c) const{
+	if((c.getX() >= tableSize) || (c.getY() >= tableSize))
+		throw std::invalid_argument( "Location outside array boundaries\n" );
+	else{
+		int x = c.getX();
+		int y = c.getY();
+
+		return board[y][x].getStatus();
+	}
+}
+
+Cell& Board::operator[](const Cell& c){
+	if((c.getX() >= tableSize) || (c.getY() >= tableSize))
+		throw std::invalid_argument( "Location outside array boundaries\n" );
+	else{
+		int x = c.getX();
+		int y = c.getY();
+
+		return board[y][x];
+	}
+}
+
+
+
---
+++ Board.h
@@ -0,0 +1,45 @@
+/*
+ * Board.h
+ *
+ *  Created on: May 3, 2018
+ *      Author: mcsa
+ */
+
+#ifndef BOARD_H_
+#define BOARD_H_
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "Cell.h"
+
+class Board{
+private:
+	//Size of the table
+	int tableSize;
+
+	//The table
+	Cell** board;
+
+	void free();
+	void create(int);
+
+public:
+	Board(int);
+	Board(const Board&);
+	~Board();
+
+    friend std::istream & operator>>(std::istream&, const Board&);
+    friend std::ostream & operator<<(std::ostream&, const Board&);
+
+    Board& operator=(const Board&);
+    Board& operator=(const char);
+
+    char operator[](const Cell&) const;
+    Cell& operator[](const Cell&);
+};
+
+
+
+#endif /* BOARD_H_ */
